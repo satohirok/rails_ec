6281379d7ace33689684b3526d18875ac3c45f18
satohirok <hiroki_g0421@yahoo.co.jp>
2024-01-02 10:45:50 +0900

    商品一覧表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# item = Item.create(:item_id => 'test2', :name => 'test2' ,:price => 2550, :description => 'test2' )
+# item.image.attach(io: File.open(Rails.root.join('app/assets/images/test1.jpg')))
+# item.save!
+
+4.times do |n|
+  item = Item.create!(
+    item_id: "test#{n + 1}",
+    name: "test#{n + 1}",
+    price: 101 + n ,
+    description: "test#{n + 1}",
+    image: ActiveStorage::Blob.create_and_upload!(io: File.open(Rails.root.join("app/assets/images/test#{n + 1}.jpg")), filename: "test#{n + 1}.jpg")
+    )
+end
