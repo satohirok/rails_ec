2f41a50092879bd73a82d35632dd3f1f27831780
satohirok <hiroki_g0421@yahoo.co.jp>
2024-01-07 11:44:40 +0900

    @latest_itemから、@itemを除外

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @latest_item = Item.order(created_at: :desc).all
+    @latest_item = Item.order(created_at: :desc).all.where.not(item_id: @item.item_id)
   end
 
   def new
