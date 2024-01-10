79a35ff9f0335883f4acd8add9f89f861caae5d8
satohirok <hiroki_g0421@yahoo.co.jp>
2024-01-10 20:38:08 +0900

    rubocop対応

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,39 +1,42 @@
-class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: 'admin', password: 'pw'
+# frozen_string_literal: true
+module Admin
+  class Admin::ProductsController < ApplicationController
+    http_basic_authenticate_with name: 'admin', password: 'pw'
 
-  def new
-    @item = Item.new
-  end
+    def new
+      @item = Item.new
+    end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
+    def edit
+      @item = Item.find(params[:id])
+    end
 
-  def index
-    @items = Item.all
-  end
+    def index
+      @items = Item.all
+    end
 
-  def create
-    @item = Item.new(item_params)
-    @item.save!
-    redirect_to admin_products_path
-  end
+    def create
+      @item = Item.new(item_params)
+      @item.save!
+      redirect_to admin_products_path
+    end
 
-  def update
-    item = Item.find(params[:id])
-    item.update!(item_params)
-    redirect_to admin_products_path
-  end
+    def update
+      item = Item.find(params[:id])
+      item.update!(item_params)
+      redirect_to admin_products_path
+    end
 
-  def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to admin_products_path
-  end
+    def destroy
+      item = Item.find(params[:id])
+      item.destroy
+      redirect_to admin_products_path
+    end
 
-  private
+    private
 
-  def item_params
-    params.require(:item).permit(:item_id, :name, :price, :description, :image)
+    def item_params
+      params.require(:item).permit(:item_id, :name, :price, :description, :image)
+    end
   end
 end
