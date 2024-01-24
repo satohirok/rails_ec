30a493e31804cd045991c90842cbe8bf84ddfd24
satohirok <hiroki_g0421@yahoo.co.jp>
2024-01-24 12:29:04 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  post '/add_item', to: 'carts#add_item', as: 'add_item'
+
   namespace :admin do
     resources :products
   end
   root to: 'items#index'
-  resources :items, except: %i[create update destroy]
+  resources :items
+  resources :carts
+  resources :item_carts
+  
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
