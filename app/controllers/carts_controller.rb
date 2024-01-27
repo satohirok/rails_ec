18e4bba6bbb6e702df0eb14f73312579f28036ed
satohirok <hiroki_g0421@yahoo.co.jp>
2024-01-27 15:58:03 +0900

    itemの情報がcurrent_cartと紐付けることができていなかったので修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,7 +6,7 @@ class CartsController < ApplicationController
 
   def index
     @total = @current_cart.total
-    @items = @current_cart.items.includes(:item_carts)
+    @items = Item.includes(:item_carts).where(item_carts: { cart_id: @current_cart.id })
     @total_price = @current_cart.total_price
   end
 
