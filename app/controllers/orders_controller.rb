efec4f2abc83aeb1ede3e936d04147d8287348cc
satohirok <hiroki_g0421@yahoo.co.jp>
2024-02-04 11:26:19 +0900

    購入処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,49 @@
 class OrdersController < ApplicationController
+  before_action :current_cart
+
   def index
   end
 
   def show
   end
 
+  def new
+    @bill = Bill.new
+  end
+
   def create
-    @bill = Bill.create(bill_params)
-    if @bill.save
+    @bill = Bill.new(bill_params)
+    @order_item= Item.joins(:item_carts).select("items.item_id, items.name, items.price, item_carts.amount").where("item_carts.cart_id = ?", @current_cart.id)
+    
+    if @current_cart.item_carts.present?
+      @bill.save
+
+      @order_item.each do |item|
+        order = Order.new(
+          item_id: item.item_id,
+          item_name: item.name,
+          item_price: item.price,
+          item_amount: item.amount,
+          item_total_price: item.price * item.amount,
+          bill: @bill  # 紐付ける
+        )
+        order.save
+      end
+
+      @item_carts = @current_cart.item_carts
+      @item_carts.delete_all
+
       flash[:notice] = '購入ありがとうございます'
       redirect_to root_path
     else
-      puts @bill.errors.full_messages
-      render :new
+      flash[:notice] = 'カートに商品を追加してください' 
+      redirect_to carts_path
     end
   end
 
   private
 
   def bill_params
-    params.permit(:last_name, :first_name, :user_name, :email, :main_adress, :sub_adress, :country, :region, :zip_code,:credit_name, :credit_number, :credit_valid, :cvv)
+    params.permit(:last_name, :first_name, :user_name, :email, :main_address, :sub_address, :country, :region, :zip_code,:credit_name, :credit_number, :credit_valid, :cvv)
   end
 end
