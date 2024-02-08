dafa343419e138560f0907958b7b9af3e8d510b6
satohirok <hiroki_g0421@yahoo.co.jp>
2024-02-08 12:24:40 +0900

    ActiveRecord::Base.transactionを適用

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,17 +10,19 @@ class Order < ApplicationRecord
     order_items = Item.joins(:item_carts).select('items.item_id, items.name, items.price, item_carts.amount').where(
       'item_carts.cart_id = ?', cart.id
     )
-
-    order_items.each do |item|
-      order = Order.new(
-        item_id: item.item_id,
-        item_name: item.name,
-        item_price: item.price,
-        item_amount: item.amount,
-        item_total_price: item.price * item.amount,
-        bill:
-      )
-      order.save
+    
+    ActiveRecord::Base.transaction do
+      order_items.each do |item|
+        order = Order.new(
+          item_id: item.item_id,
+          item_name: item.name,
+          item_price: item.price,
+          item_amount: item.amount,
+          item_total_price: item.price * item.amount,
+          bill:
+        )
+        order.save
+      end
     end
     
     CheckoutMailer.confirm_mail(bill, bill.orders).deliver
